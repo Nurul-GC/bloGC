c1ab62914edc7ed08766e9698eeef33144485e21
Nurul-GC <Nurul-GC@github.com>
2021-03-12 21:40:28 +0200

    updating commit

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,25 @@
 from flask import Flask
 from app.config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+# importando bootstrap para o projecto
 # from flask_bootstrap import Bootstrap
 
+# definindo a instancia do flask
+# com as definicoes do projecto
 app = Flask(__name__)
+
+# definindo as configuracoes do projecto
+# importando-as do modulo config
 app.config.from_object(Config)
-# app.config['SECRET_KEY'] = 'ANGOLACKERS_ACADEMY'
+
+# definindo as configuracoes de db
+# para o projecto
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+# iniciando a instancia do bootstrap
 # Bootstrap(app=app)
 
-from app import routes
+from app import routes, models
--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,6 @@
 from app import *
 from app.forms import LoginForm
 from flask import flash, redirect, render_template
-from flas
 
 
 @app.route('/')
@@ -15,6 +14,6 @@ def login():
     form = LoginForm()
     if form.validate_on_submit():
         # Login requested for user {form.username.data}, remember_me={form.remember_me.data}
-        flash(f'Ola {form.username.data}...')
+        flash(f'Bem-Vindo de volta {form.username.data}...')
         return redirect('/')
     return render_template('login.html', title='Angolackers-login', form=form)
