ebc8b0296c14e131949e8a0e304c8d0058603b36
Nurul-GC <Nurul-GC@github.com>
2021-03-12 19:50:47 +0200

    updating commit

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
 from app import *
 from app.forms import LoginForm
 from flask import flash, redirect, render_template
+from flas
 
 
 @app.route('/')
@@ -9,10 +10,11 @@ def index():
     return render_template("index.html", title='Angolackers')
 
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        flash(f'Login requested for user {form.username.data}, remember_me={form.remember_me.data}')
-        return redirect('/index')
+        # Login requested for user {form.username.data}, remember_me={form.remember_me.data}
+        flash(f'Ola {form.username.data}...')
+        return redirect('/')
     return render_template('login.html', title='Angolackers-login', form=form)
--- git_init.sh
+++ git_init.sh
@@ -4,7 +4,7 @@
 
 git init # criando a pasta .git
 git add ./* # adicionando todos os arquivos existentes na pasta atual ao repositorio
-git commit -m "updating commit" # criando o primeiro commit
+git commit -m "updating commit" # criando o primeiro commit (comentario)
 git remote add origin https://github.com/Nurul-GC/angolackers-msg.git # conectando com o repositorio origem
 git branch -M main # conectando com o ramo principal (default main)
 git push -u origin main # fazendo o upload (push)
