01c9d5df5b0c0a2f31ca0a2ff76734dd605f4154
Nurul-GC <Nurul-GC@github.com>
2021-03-13 17:17:59 +0200

    updating commit

--- app/forms.py
+++ app/forms.py
@@ -1,10 +1,19 @@
 from flask_wtf import FlaskForm
 from wtforms import BooleanField, PasswordField, StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Email, EqualTo
 
 
 class LoginForm(FlaskForm):
+    """formulario para login"""
     username = StringField('Nome Usuario', validators=[DataRequired()])
     password = PasswordField('Senha', validators=[DataRequired()])
     remember_me = BooleanField('Lembrar-me')
     submit = SubmitField('Entrar')
+
+class SigninForm(FlaskForm):
+    """formulario para signin"""
+    username = StringField('Nome Usuario', validators=[DataRequired()])
+    useremail = StringField('Email Usuario', validators=[DataRequired(), Email()])
+    password1 = PasswordField('Senha Usuario', validators=[DataRequired()])
+    password2 = PasswordField('Reintroduza Senha', validators=[DataRequired(), EqualTo('password1')])
+    submit = SubmitField('Cadastrar')
--- app/routes.py
+++ app/routes.py
@@ -1,12 +1,13 @@
 from app import *
-from app.forms import LoginForm
+from app.forms import LoginForm, SigninForm
 from flask import flash, redirect, render_template
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template("index.html", title='Angolackers')
+    form = LoginForm()
+    return render_template("index.html", title='Angolackers', form=form)
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -16,4 +17,16 @@ def login():
         # Login requested for user {form.username.data}, remember_me={form.remember_me.data}
         flash(f'Bem-Vindo de volta {form.username.data}...')
         return redirect('/')
-    return render_template('login.html', title='Angolackers-login', form=form)
+    elif form.username != None:
+        flash(f'Relaxa {form.username.data} já tens a sessão iniciada...')
+        return redirect('/')
+    return render_template("login.html", title="Angolackers-InicioSessão", form=form)
+
+@app.route('/signin', methods=['GET', 'POST'])
+def signin():
+    form = SigninForm()
+    if form.validate_on_submit():
+        # Signin requested for user {form.username.data}, remember_me={form.remember_me.data}
+        flash(f'Bem-Vindo {form.username.data}, o seu cadastro foi bem sucedido...')
+        return redirect('/')
+    return render_template("signin.html", title="Angolacker-Cadastro", form=form)
