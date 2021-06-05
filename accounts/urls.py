a966ea4558eb65cd8cfccadb9391bd94cc4e01f4
Nurul-GC <nuruldecarvalho@gmail.com>
2021-06-06 00:31:05 +0200

    updating commit

--- accounts/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from accounts import views
-
-app_name = 'accounts'
-urlpatterns = [
-    path('cadastro/', views.cadastro, name='cadastro')
-]
--- accounts/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-
-
-# Create your views here.
-def cadastro(request):
-    return render(request, 'registration/cadastro.html')
