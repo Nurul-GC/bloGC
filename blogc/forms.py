bc7b2de5558039d04936c2d985a0fb85bc67c4a8
Nurul-GC <nuruldecarvalho@gmail.com>
2021-06-08 01:53:51 +0200

    updating commit

--- blogc/forms.py
+++ blogc/forms.py
@@ -1,8 +1,1 @@
 from django import forms
-
-
-class PartilharPublicacaoForm(forms.Form):
-    nome = forms.CharField(max_length=25)
-    email = forms.EmailField()
-    destinatario = forms.EmailField()
-    comentario = forms.CharField(required=False, widget=forms.Textarea)
