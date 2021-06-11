e6e0e5d5072f4e4f6f40166be828a050cbf66467
Nurul-GC <nuruldecarvalho@gmail.com>
2021-06-11 20:35:54 +0200

    updating commit

--- blogc/urls.py
+++ blogc/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('publicados/', views.publicados, name='publicados'),
     path('rascunhos/', views.rascunhos, name='rascunhos'),
-    path('publicacao/<slug:_subtitulo>/', views.detalhes, name='detalhes'),
+    path('<slug:_subtitulo>/', views.detalhes, name='detalhes'),
+    path('tag/<slug:tag_subtitulo>/',  views.publicados, name='tags'),
+    path('editar/<slug:_subtitulo>/', views.editar, name='editar')
 ]
